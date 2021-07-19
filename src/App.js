03fc2e30b835be68fc25b87dc3b9424f09df1328
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2021-07-19 22:53:29 +1000

    update on App

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is an awsome project in React 
         </a>
       </header>
     </div>
