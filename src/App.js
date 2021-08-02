205b2760691528587b4ccc524f1fea60f0863bac
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2021-08-02 21:55:58 +1000

    update on 20210802

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Song from './components/Song';
+import Player from './components/Player';
+import data from './util';
+import  './sass/app.scss';
 
 
 function App() {
+  //State
+  // data() returns whole chillihop data
+  //songs is a state has all the chilhop object properties and values
+const [songs, setSongs] = useState(data());
+const [currentSong, setCurrentSong] = useState(songs[4]);
   return (
     <div className="App">
-     <h1>This is my music add</h1>
+     <Song currentSong={currentSong} />
+     <Player/>
+     
     </div>
   );
 }
